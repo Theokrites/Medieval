c2e6f55293a4ca33bb04a40638d1d3fb1055a79d
Theokrites <74838406+Theokrites@users.noreply.github.com>
2020-12-21 17:58:09 -0600

    created a person class with theism for lols

--- anthropos.py
+++ anthropos.py
@@ -1,1 +1,28 @@
+class Anthropos:
+
+    def slay(self):
+        self.isAlive = False
+        if self.isTheist:
+            self.inAfterLife = True
+
+    def beget(self, other):
+        other.isAlive = True
+        self.children.append(other)
+
+    def __init__(self):
+        self.isAlive = True
+        self.children = []
+        self.inAfterLife = False
+        self.isTheist = True
+
+    def commitApostasy(self):
+        self.isTheist = False
+
+
+
+
+
+dan = Anthropos()
+dan.commitApostasy()
+dan.slay()
 
